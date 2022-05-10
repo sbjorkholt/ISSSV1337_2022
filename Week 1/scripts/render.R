505973ad12a017a824a173abcff4164a1b39f561
Solveig Bjørkholt <solvebjo@uio.no>
2022-05-10 11:42:28 +0200

    initial scripts

---
+++ Week 1/scripts/render.R
@@ -0,0 +1,5 @@
+
+setwd("C:/Users/solvebjo/OneDrive - Universitetet i Oslo/Teaching/ISSSV1337/ISSSV1337/Week 1/scripts")
+
+rmarkdown::render('Day1.Rmd', output_dir = "../.")
+rmarkdown::render('Day2.Rmd', output_dir = "../.")
